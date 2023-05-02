6c10657fad1afa64297ff075484162c2577e9ebd
monjurur <monjurur.roshid@efgt.eurofoods-bd.com>
2023-05-02 16:22:47 +0600

    Initial commit

---
+++ lib/Device_info.dart
@@ -0,0 +1,56 @@
+import 'package:dart_ipify/dart_ipify.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+
+class DeviceInfo extends StatefulWidget {
+  const DeviceInfo({Key? key}) : super(key: key);
+
+  @override
+  State<DeviceInfo> createState() => _DeviceInfoState();
+}
+
+class _DeviceInfoState extends State<DeviceInfo> {
+  NetworkInfo networkInfo = NetworkInfo();
+  DeviceInfoPlugin deviceInfo =DeviceInfoPlugin();
+  WindowsDeviceInfo? windowsDeviceInfo;
+  AndroidDeviceInfo? androidDeviceInfo;
+
+String ip ="";
+  Future<WindowsDeviceInfo>getWindowsInfo()async{
+    return await deviceInfo.windowsInfo;
+  }
+  Future<AndroidDeviceInfo>getAndroidInfo()async{
+    return await deviceInfo.androidInfo;
+  }
+
+  Widget showCard(String name , String value){
+    return   Card(child: ListTile(title: Text("$name: $value"),),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(
+        child:FutureBuilder<WindowsDeviceInfo>(
+          future: getWindowsInfo(),
+          builder:(context, snapshot) {
+          return Column(children: [
+showCard('Device Id',"${snapshot.data!.deviceId}"),
+showCard('Device Name',"${snapshot.data!.computerName}"),
+//showCard('Device Model',"${snapshot.data!.}"),
+            showCard('Network Ip',"${ip}"),
+
+            TextButton(onPressed: ()async{
+            final ipv4 =await Ipify.ipv4();
+            ip = ipv4;
+            setState(() {
+
+            });
+          }, child: Text("Get Ip")),
+          ],);
+        },
+        )
+    );
+  }
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import network_info_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+}
